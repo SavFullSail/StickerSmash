6998416622638517103a5d7cc494ba7fbfb645fc
Savar Sethi <savar.sethi@gmail.com>
2026-08-16 11:43:06 +0530

    fix: refactor iOS deployment target configuration and add plugin for Podfile modification

---
+++ plugins/withIosDeploymentTargetFix.js
@@ -0,0 +1,37 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+const MIN_DEPLOYMENT_TARGET = '15.1';
+
+function withIosDeploymentTargetFix(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      let contents = fs.readFileSync(podfilePath, 'utf-8');
+
+      const patch = `    # @generated withIosDeploymentTargetFix
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |build_config|
+        if Gem::Version.new(build_config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] || '0') < Gem::Version.new('${MIN_DEPLOYMENT_TARGET}')
+          build_config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '${MIN_DEPLOYMENT_TARGET}'
+        end
+      end
+    end
+`;
+
+      if (!contents.includes('@generated withIosDeploymentTargetFix')) {
+        contents = contents.replace(
+          /post_install do \|installer\|/,
+          `post_install do |installer|\n${patch}`
+        );
+        fs.writeFileSync(podfilePath, contents);
+      }
+
+      return config;
+    },
+  ]);
+}
+
+module.exports = withIosDeploymentTargetFix;
